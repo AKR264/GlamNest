7c674c5c3b6fbe5a751c75e95949620ea02abaf2
Anamika Rao <anamikarao264@gmail.com>
2025-07-15 00:53:56 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-// // import logo from "./logo.svg";
-// import "./App.css";
-// import Navbar from "./Navbar";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import Category from "./components/Category";
-// import About from "./components/About";
-// import Products from "./components/Products";
-// import AllProducts from "./components/AllProducts";
-// import MuiLogin from "./components/MuiLogin";
-// import MuiRegister from "./components/MuiRegister";
-
-// function App() {
-//   return ( 
-//     <>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/Category" element={<Category />} />
-//           <Route path="/Products/:categoryName" element={<Products />} />
-//           <Route path="/Products" element={<AllProducts />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/MuiLogin" element={<MuiLogin/>} />
-//           <Route path="/MuiRegister" element={<MuiRegister/>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
 // import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Navbar";
